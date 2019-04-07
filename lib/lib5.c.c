14c158e4ea938aa173f4b47a08692941f513904b
Nailaa <49157797+Nailaamani@users.noreply.github.com>
2019-04-07 12:50:08 +0700

    Add files via upload

---
+++ lib/lib5.c.c
@@ -0,0 +1,46 @@
+//***********************************************************//
+//                      [ Source Code ]
+//
+// Institution       : Institut Teknologi Bandung
+// Name              : Naila Amani
+// File Name         : lib5.c
+// Dependency        : stdio.h
+//
+// Description:
+//     lib5.c berisi tentang perintah PrintLayout
+//
+// Status:
+// 1. Naila Amani - 13217012 : Create the file
+//***********************************************************//
+
+#include <stdio.h>
+
+
+void PrintLayout(pcb_t pcb)
+{	//melakukan proses mencetak/print hasil layout pada pcb
+
+	//KAMUS LOKAL
+	int i,j;
+    
+	
+	//ALGORITMA
+	printf("   ");
+	
+    for(i=0; i<pcb.panjang; i++)		 
+    {
+    	printf(" %3d ", i+1); //Sebagai nomer 1,2,3.. memanjang
+	}
+	printf("\n");
+
+
+	for(j=0; j<pcb.lebar; j++)
+    {
+    	printf(" %3d ", j+1); //Sebagai nomer 1,2,3.. melebar
+    	for(i=0; i<pcb.panjang; i++)
+        {
+			printf(" %3s ", pcb.layout[i][j]); //mencetak hasil layout
+		}
+	printf("\n");
+	}
+
+}
