b6a4b44c20f100cc33e15542a01e015271335c3d
Nailaa <49157797+Nailaamani@users.noreply.github.com>
2019-04-07 14:14:13 +0700

    Add files via upload

---
+++ lib/lib7.c
@@ -0,0 +1,91 @@
+//***********************************************************//
+//                      [ Source Code ]
+//
+// Institution       : Institut Teknologi Bandung
+// Name              : Naila Amani
+// File Name         : lib7.c
+// Dependency        : stdio.h, stdlib.h, string.h
+//
+// Description:
+//     lib7.c berisi tentang perintah saving pada CSV.
+// Akan terdapat 2 perintah yaitu, save untuk csv layout dan
+// csv routing.
+//
+// Status:
+// 1. Naila Amani - 13217012 : Create the file
+//***********************************************************//
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+void SaveLayout(char *filename, pcb_t pcb)
+{
+	
+	//membuka file eksternal
+	FILE *fp;
+	char *fileLayout=(char*)calloc(20,sizeof(char));
+	
+	//membuat nama file eksternal dengan menggabungkan
+	//inputan namafile dengan _layout.csv
+	strcat(fileLayout,filename);
+	strcat(fileLayout,"_layout.csv");
+	
+	fp=fopen(fileLayout,"w+");
+ 
+	int i,j;
+	
+	for(j=0;j<pcb.lebar;j++)
+		{
+    	for(i=0;i<pcb.panjang;i++)
+			{
+			fprintf(fp,"%s,",pcb.layout[i][j]);
+    		}
+    		fprintf(fp,"\n");
+		}
+	fclose(fp);
+
+	printf("\n Layout disimpan dalam berkas %s", fileLayout);
+	printf("\n");
+	printf("\n %s file berhasil disimpan",fileLayout);
+
+	free(fileLayout);
+
+}
+
+
+void SaveRouting(char *filename, pcb_t pcb)
+{	
+	//membuka file eksternal
+	FILE *fp;
+	char *fileRouting=(char*)calloc(20,sizeof(char));
+	
+	//membuat nama file eksternal dengan menggabungkan
+	//inputan namafile dengan _layout.csv
+	strcat(fileRouting,filename);
+	strcat(fileRouting,"_routing.csv");
+	
+	fp=fopen(fileRouting,"w+");
+ 
+ 	//kamus lokal
+	int i,j;
+
+
+	for(j=0;j<pcb.lebar;j++)
+		{
+	    	for(i=0;i<pcb.panjang;i++)
+			{
+			fprintf(fp,"%c,",pcb.routing[i][j]);
+    		}
+    	fprintf(fp,"\n");
+		}
+	fclose(fp);
+
+	printf("\n Routing disimpan dalam berkas %s", fileRouting);
+	printf("\n");
+	printf("\n %s file berhasil disimpan",fileRouting);
+
+	free(fileRouting);
+
+}
