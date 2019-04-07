70401b3d6a46f0b44724cb8cf06e2a9cf14e7e7d
Nailaa <49157797+Nailaamani@users.noreply.github.com>
2019-04-07 14:36:52 +0700

    Update lib5.c

--- lib/lib5.c
+++ lib/lib5.c
@@ -7,7 +7,7 @@
 // Dependency        : stdio.h
 //
 // Description:
-//     lib5.c berisi tentang perintah PrintLayout
+//     lib5.c berisi tentang perintah PrintLayout dan PrintRouting.
 //
 // Status:
 // 1. Naila Amani - 13217012 : Create the file
@@ -16,8 +16,8 @@
 #include <stdio.h>
 
 
-void PrintLayout(pcb_t pcb)
-{	//melakukan proses mencetak/print hasil layout pada pcb
+void PrintLayout(pcb_t pcb){
+//melakukan proses mencetak/print hasil layout pada pcb
 
 	//KAMUS LOKAL
 	int i,j;
@@ -26,27 +26,31 @@ void PrintLayout(pcb_t pcb)
 	//ALGORITMA
 	printf("   ");
 	
-    for(i=0; i<pcb.panjang; i++)		 
-    {
-    	printf(" %3d ", i+1); //Sebagai nomer 1,2,3.. memanjang
+	for(i=0; i<pcb.panjang; i++)		 
+    	{
+    		printf(" %3d ", i+1); //Sebagai nomer 1,2,3.. memanjang
 	}
+	
+	
 	printf("\n");
-
-
+	
+	
 	for(j=0; j<pcb.lebar; j++)
-    {
-    	printf(" %3d ", j+1); //Sebagai nomer 1,2,3.. melebar
-    	for(i=0; i<pcb.panjang; i++)
-        {
-			printf(" %3s ", pcb.layout[i][j]); //mencetak hasil layout
+ 	{
+    		printf(" %3d ", j+1); //Sebagai nomer 1,2,3.. melebar
+    		for(i=0; i<pcb.panjang; i++)
+        	{
+		//mencetak hasil layout
+		printf(" %3s ", pcb.layout[i][j]); 
 		}
 	printf("\n");
 	}
 
 }
 
-void PrintRouting(pcb_t pcb)
-{	//melakukan proses mencetak/print hasil routing pada pcb
+
+void PrintRouting(pcb_t pcb){	
+//melakukan proses mencetak/print hasil routing pada pcb
 
 	//KAMUS LOKAL
 	int i,j; 
@@ -55,17 +59,18 @@ void PrintRouting(pcb_t pcb)
 	printf("   ");
 	
     for(i=0; i<pcb.panjang; i++)
-    {
+	{
     	printf(" %3d ", i+1); //Sebagai nomer 1,2,3.. memanjang
 	}
 	printf("\n");
 
 	for(j=0; j<pcb.lebar; j++)
-    {
-    	printf(" %3d ", j+1);	//Sebagai nomer 1,2,3.. melebar
+		{
+    		printf(" %3d ", j+1);	//Sebagai nomer 1,2,3.. melebar
 	    	for(i=0; i<pcb.panjang; i++)
-      	  	{
-				printf(" %3c ", pcb.routing[i][j]);  //mencetak hasil routing
+      	  		{
+			//mencetak hasil routing
+			printf(" %3c ", pcb.routing[i][j]); 
 			}
 		printf("\n");
 	}
