fe3261e614c006cce559a4bc537dd5897770331a
Nailaa <49157797+Nailaamani@users.noreply.github.com>
2019-04-07 13:19:24 +0700

    Update lib6.c

--- lib/lib6.c
+++ lib/lib6.c
@@ -7,38 +7,70 @@
 // Dependency        : stdio.h
 //
 // Description:
-//     lib5.c berisi tentang perintah PrintRouting
-//
+//     lib6.c berisi tentang perintah mereset layout
+// dan routing pada pcb
 // Status:
 // 1. Naila Amani - 13217012 : Create the file
 //***********************************************************//
 
 #include <stdio.h>
 
+void ResetRouting(pcb_t pcb)
+{	//melakukan proses mereset Routing pada PCB
 
-void PrintRouting(pcb_t pcb)
-{	//melakukan proses mencetak/print hasil layout pada pcb
+	int i,j;
 
-	//KAMUS LOKAL
-	int i,j; 
-
- 	 	
-	//ALGORITMA
 	printf("   ");
 	
-    for(i=0; i<pcb.panjang; i++)
-    {
-    	printf(" %3d ", i+1); //Sebagai nomer 1,2,3.. memanjang
-	}
+	//menuliskan nomer 1,2,3.. memanjang
+    	for(i=0; i<pcb.panjang; i++)
+    		{
+    		printf(" %3d ", i);
+		}
+	
 	printf("\n");
 
+	
 	for(j=0; j<pcb.lebar; j++)
-    {
-    	printf(" %3d ", j+1);	//Sebagai nomer 1,2,3.. melebar
-	    	for(i=0; i<pcb.panjang; i++)
-      	  	{
-				printf(" %3c ", pcb.routing[i][j]);  //mencetak hasil routing
+    		{
+    		printf(" %3d ", j+1); //menuliskan 1,2,3... melebar
+    		for(i=0; i<pcb.panjang; i++)
+        		{
+        		pcb.routing[i][j]=' ';
+			//mengosongkan routing
+			printf(" %3c ", pcb.routing[i][j]); 
+			//mencetak hasil pengosongan
 			}
 		printf("\n");
 	}
+}
+
+
+void ResetLayout(pcb_t pcb)
+{
+	int i,j;
+    
+ 	printf("   ");
+	
+	//penomeran 1,2,3 memanjang pada tabel tampilan
+	for(i=0; i<pcb.panjang; i++)
+    	{
+    	printf(" %3d ", i+1);
+	}
+	
+	printf("\n");
+
+	for(j=0; j<pcb.lebar; j++)
+    	{
+    	printf(" %3d ", j+1); //penomeran 1,2,3 melebar
+	    	for(i=0; i<pcb.panjang; i++)
+        	{
+			//mereset layout pada layout
+        		strcpy (pcb.layout[i][j]," "); 
+			//mencetak layout hasil reset
+			printf(" %3s ", pcb.layout[i][j]);
+		}
+	printf("\n");
+	}
+
 }
